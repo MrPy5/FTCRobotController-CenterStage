99cb9a77d17d47cdc6d6a5d311e9a22adbeb1edb
MrPy5 <kalya.kothavale@gmail.com>
2024-02-07 21:35:38 -0600

    UPDATE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Game/Autonomous/MeetAutos/FunctionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Game/Autonomous/MeetAutos/FunctionTest.java
@@ -24,6 +24,7 @@ public class FunctionTest extends AutoControlsCombined {
             driveOne.add(new Strafe(new MillisecondTrigger(0), 25, 0.5, 0, -1));
             driveOne.add(new Drive(new IndexTrigger(0, driveOne), 85, 1, 0));
         driveOne.Start(0);
+        sleep(5000);
 
     }
 
